7d182fa49a19de5d8747fa623baf188a6c9361bf
SarahSquire <sarah@engageidentity.com>
2015-06-10 15:58:28 -0700

    initializing

---
+++ server.js
@@ -0,0 +1,65 @@
+// server.js
+// call the packages we need
+var express    = require('express');        
+var app        = express();                 
+var bodyParser = require('body-parser');
+var r = require('./jsrsasign.js');
+var fs = require('fs');
+var rsa = require('node-rsa');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'welcome to the api!' });   
+});
+
+
+
+// on routes that end in /mvcr
+var pemtext;
+router.route('/mvcr')
+.post(function(req, res) {
+
+	// Header
+	var oHeader = {alg: 'RS256', typ: 'JWT'};
+	var sHeader = JSON.stringify(oHeader);
+	// Payload
+	var oPayload = req.body.payload;
+	//read key
+
+	//sign jwt
+	var sJWT = KJUR.jws.JWS.sign("RS256", sHeader, oPayload, app.okey);
+	// Return JWT
+	res.json(sJWT);           
+});
+
+// register routes
+app.use('/api', router);
+
+fs.readFile('key.jwk', 'utf8', function (err, data) {
+	if (err) throw err;
+
+	var key = JSON.parse(data);
+	app.okey = r.KEYUTIL.getKey(key);
+
+	// start server
+	app.listen(port);
+	console.log('Magic happens on port ' + port);
+
+});
+
+
