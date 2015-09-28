aebccfb19228bbd48062c2aa9d200b81316cad5a
magician2015 <bss.dev.tanle@gmail.com>
2015-09-28 23:00:46 +0700

    Fix server api

--- server.js
+++ server.js
@@ -29,8 +29,7 @@ router.use(function(req, res, next) {
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-//    res.json({ message: 'welcome to the api!' });
-  res.sendFile("index.html");
+    res.json({ message: 'welcome to the api!' });
 });
 
 
